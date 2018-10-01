526dbb2f1b828b82262c9f99fc146f1d2b5c5a4c
unknown <tansuta>
2018-10-01 09:54:17 +0700

    db

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var pgp = require('pg-promise')(/*options*/)
-var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true')
+var db = pgp('postgres://weaclbeiwbxfsq:27abbf9f549e54ea47de0b0e387e2d77cea04352f95426ccc713a672b1fcdb65@ec2-54-243-147-162.compute-1.amazonaws.com:5432/d1himi9nqbifnr?ssl=true')
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -9,19 +9,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(express.static('static'));
 app.set('view engine', 'ejs');
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.render('pages/index');
 });
 
-app.get('/index',function(req,res){
+app.get('/index', function (req, res) {
     res.render('pages/index');
 });
 
-app.get('/about',function(req,res){
+app.get('/about', function (req, res) {
     var name = 'Tansuta Ratkaew';
-    var hobbies = ['Music','Movie','Programming'];
+    var hobbies = ['Music', 'Movie', 'Programming'];
     var bdate = '09/08/1977'
-    res.render('pages/about',{fullname : name,hobbies : hobbies,bdate  });
+    res.render('pages/about', { fullname: name, hobbies: hobbies, bdate });
 });
 // Display all products
 app.get('/products', function (req, res) {
@@ -30,40 +30,40 @@ app.get('/products', function (req, res) {
     var id = req.param('id');
     var sql = 'select * from products';
     if (id) {
-        sql += ' where id ='+id;
-        }
+        sql += ' where id =' + id;
+    }
     db.any(sql)
-    .then(function(data){
-        console.log('DATA:'+data);
-        res.render('pages/products',{products:data})
-        
-    })
-    .catch(function(error){
-        console.log('ERROR:'+error);
-        
-    })
+        .then(function (data) {
+            console.log('DATA:' + data);
+            res.render('pages/products', { products: data })
+
+        })
+        .catch(function (error) {
+            console.log('ERROR:' + error);
+
+        })
 
 });
 
-app.get('/products/:pid',function(req,res){
+app.get('/products/:pid', function (req, res) {
     var pid = req.params.pid;
     var sql = "select * from products where id =" + pid;
-   
-    db.any(sql)
-    .then(function(data){
-        
-        res.render('pages/product_edit',{product: data[0] })
-        
-    })
-    .catch(function(error){
-        console.log('ERROR:'+error);
-        
-    })
 
-   
-});  
+    db.any(sql)
+        .then(function (data) {
+
+            res.render('pages/product_edit', { product: data[0] })
+
+        })
+        .catch(function (error) {
+            console.log('ERROR:' + error);
+
+        })
+
+
+});
 //update data
-app.post('/product/update', function(req, res) {
+app.post('/product/update', function (req, res) {
     var id = req.body.id;
     var title = req.body.title;
     var price = req.body.price;
@@ -74,40 +74,40 @@ app.post('/product/update', function(req, res) {
 
 
 
-});  
-
-
-
-app.get('/users/:id', function(req, res) {
-var id = req.params.id;
-var sql = 'select * from users';
-    if(id){
-        sql += ' where id ='+id;
-    }
-  db.any(sql)
-  .then(function(data){
-      console.log('DATA:' + data);
-      res.render('pages/users',{users :data})
-
-  })
-  .catch(function(error){
-    console.log('ERROR:'+ error);
-})
 });
 
-  app.get('/users',function(req,res){
-    db.any('select * from users',)
-    .then(function(data){
-        console.log('DATA:' + data);
-        res.render('pages/users',{users :data})      
-    })  
 
-    .catch(function(error){
-        console.log('ERROR:'+ error);
-    })
-     });                        
 
-     var port = process.env.PORT || 8080;
-     app.listen(port, function() {
-     console.log('App is running on http://localhost:' + port);
-     });
+app.get('/users/:id', function (req, res) {
+    var id = req.params.id;
+    var sql = 'select * from users';
+    if (id) {
+        sql += ' where id =' + id;
+    }
+    db.any(sql)
+        .then(function (data) {
+            console.log('DATA:' + data);
+            res.render('pages/users', { users: data })
+
+        })
+        .catch(function (error) {
+            console.log('ERROR:' + error);
+        })
+});
+
+app.get('/users', function (req, res) {
+    db.any('select * from users', )
+        .then(function (data) {
+            console.log('DATA:' + data);
+            res.render('pages/users', { users: data })
+        })
+
+        .catch(function (error) {
+            console.log('ERROR:' + error);
+        })
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+    console.log('App is running on http://localhost:' + port);
+});
