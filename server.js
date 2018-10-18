9f17ca112ab9d1e606d49e29a057909633a83fc6
unknown <tansuta>
2018-10-18 09:38:15 +0700

    test

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.post('/product/update', function (req, res) {
     var id = req.body.id;
     var title = req.body.title;
     var price = req.body.price;
-    var sql = `update products set title = ${title},price = ${price} where id = ${id}`;
+    var sql = `update products set title = '${title}',price = ${price} where id = ${id}`;
     //db.none
     db.any(sql)
     .then(function (data) {
