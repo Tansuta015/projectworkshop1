484877624477e492c7c98a0f0c0ba64c711e7200
unknown <tansuta>
2018-10-18 08:51:11 +0700

    add

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get('/addnewproduct', function(req,res){
     var title = req.body.title;
     var price = req.body.price;
     var sql = `INSERT INTO products(id, title, price)
-    VALUES (${id}, ${title}, ${price});`;
+    VALUES ('${id}', '${title}', '${price}');`;
     //db.none
     console.log('UPDATE:' + sql);
     res.send(sql);
