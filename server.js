0ed904e92f07e4262e3f486cb6cc30dbda350017
tansuta <gilf_512@hotmail.com>
2018-10-22 14:55:59 +0700

    report

--- server.js
+++ server.js
@@ -237,8 +237,8 @@ app.post('/addnewuser', function (req, res) {
 
 // report_product
 app.get('/report_product', function (req, res) {
- 
-    var sql = 'select * from products order by price DESC limit 10';
+    var sql = 'SELECT title, SUM(price) AS price FROM products GROUP BY ROLLUP (title) ORDER BY title';
+    //   var sql = 'select * from products order by price DESC limit 10';
 
     db.any(sql)
         .then(function (data) {
